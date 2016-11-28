562c8a48106dd2d29268e0c090b34f9692b5772c
russvetsper <russvetsper@gmail.com>
2016-11-27 18:49:44 -0800

    add tables in SQL

--- src/GummiBearKingdom/Migrations/20161128022943_MakeTableNamesPlural.cs
+++ src/GummiBearKingdom/Migrations/20161128022943_MakeTableNamesPlural.cs
@@ -1,19 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore.Migrations;
+using GummiBearKingdom.Models;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace GummiBearKingdom.Migrations
 {
-    public partial class MakeTableNamesPlural : Migration
+    namespace GummiBearKingdom.Migrations
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
+        [DbContext(typeof(GummiBearKingdomContext))]
+        [Migration("20161128022540_Initial")]
+        partial class Initial
         {
+            protected override void BuildTargetModel(ModelBuilder modelBuilder)
+            {
+                modelBuilder
+                    .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                    .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-        }
+                modelBuilder.Entity("GummiBearKingdom.Models.Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
+                    b.Property<string>("Author");
 
+                    b.Property<string>("Text");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("BlogId");
+
+                    b.ToTable("Blog");
+                });
+
+                modelBuilder.Entity("GummiBearKingdom.Models.Product", b =>
+                {
+                    b.Property<int>("ProductsId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Cost");
+
+                    b.Property<string>("Coo");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("ProductsId");
+
+                    b.ToTable("Products");
+                });
+            }
         }
     }
-}
