e1d74f0842060e7b9187b8fd964491ab7bea2ac9
russvetsper <russvetsper@gmail.com>
2016-11-27 18:37:55 -0800

    run migration command to generate tables

---
+++ src/GummiBearKingdom/Migrations/20161128022540_Initial.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace GummiBearKingdom.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductsId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Coo = table.Column<string>(nullable: true),
+                    Cost = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductsId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blog",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Author = table.Column<string>(nullable: true),
+                    ProductsId = table.Column<int>(nullable: true),
+                    Text = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blog", x => x.BlogId);
+                    table.ForeignKey(
+                        name: "FK_Blog_Products_ProductsId",
+                        column: x => x.ProductsId,
+                        principalTable: "Products",
+                        principalColumn: "ProductsId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blog_ProductsId",
+                table: "Blog",
+                column: "ProductsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blog");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ src/GummiBearKingdom/Migrations/GummiBearKingdomContextModelSnapshot.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using GummiBearKingdom.Models;
+
+namespace GummiBearKingdom.Migrations
+{
+    [DbContext(typeof(GummiBearKingdomContext))]
+    partial class GummiBearKingdomContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("GummiBearKingdom.Models.Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Author");
+
+                    b.Property<int?>("ProductsId");
+
+                    b.Property<string>("Text");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("BlogId");
+
+                    b.HasIndex("ProductsId");
+
+                    b.ToTable("Blog");
+                });
+
+            modelBuilder.Entity("GummiBearKingdom.Models.Product", b =>
+                {
+                    b.Property<int>("ProductsId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Coo");
+
+                    b.Property<int>("Cost");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("ProductsId");
+
+                    b.ToTable("Products");
+                });
+
+            modelBuilder.Entity("GummiBearKingdom.Models.Blog", b =>
+                {
+                    b.HasOne("GummiBearKingdom.Models.Product")
+                        .WithMany("Blogs")
+                        .HasForeignKey("ProductsId");
+                });
+        }
+    }
+}
